7f201a0799b6ab4c1fcde53bf56278bbdcb6a6a8
IanMFrost <ianmfrost0@gmail.com>
2018-04-16 12:45:38 +0200

    Res render virker

--- app/app.js
+++ app/app.js
@@ -9,6 +9,8 @@ app.set('appData', dataFile);
 app.set('view engine', 'ejs');
 app.set('views', './views');
 
+
+
 app.use(express.static('app/public'));
 app.use(require('./routes/index'));
 app.use(require('./routes/speakers'));
