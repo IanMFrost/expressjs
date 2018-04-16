4ce75a6726d444127fd1048d8030871d5710cf57
IanMFrost <ianmfrost0@gmail.com>
2018-04-16 10:42:16 +0200

    lavet express

--- app/app.js
+++ app/app.js
@@ -1,10 +1,26 @@
-var http = require('http');
+var express = require('express');
+var app = express();
+app.get('/',function(req, response){
+    res.send('<h1> Roux Academy Meetups </h1>');
 
-var myServer = http.createServer(function(request, response) {
-  response.writeHead(200, {"Content-Type" : "text/html"});
-  response.write('<h1>Roux Meetups</h1>');
-  response.end();
 });
 
-myServer.listen(3000);
-console.log('Go to http://localhost:3000 on your browser');
+var server = app.listen(3000, function() {
+    console.log('listening on port 3000' + app.get());
+});
+
+
+
+
+
+
+// var http = require('http');
+
+// var myServer = http.createServer(function(request, response) {
+//   response.writeHead(200, {"Content-Type" : "text/html"});
+//   response.write('<h1>Roux Meetups</h1>');
+//   response.end();
+// });
+
+// myServer.listen(3000);
+// console.log('Go to http://localhost:3000 on your browser');
